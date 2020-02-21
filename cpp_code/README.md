a6c6e0702ce90b852effcee087287835b7751b59
Cuda Chen <clh960524@gmail.com>
2020-02-22 07:02:55 +0800

    finish workable C++ code

---
+++ cpp_code/README.md
@@ -0,0 +1,4 @@
+# Compile
+```
+$ g++ -o opencv_dnn_cuda_test_cpp main.cpp -I/usr/local/include/opencv4 -lopencv_core -lopencv_dnn -lopencv_highgui -lopencv_imgcodecs
+```
--- cpp_code/main.cpp
+++ cpp_code/main.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <chrono>
 
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/core.hpp"
 #include "opencv2/dnn.hpp"
-
+/*
 using namespace std;
 using namespace cv;
 using namespace dnn;
+*/
 
 int main()
 {
 	double confidenceThreshold = 0.5;
 	double NMSThreshold = 0.4;
 	int numClasses = 80;
-	clock_t start, end;
 
-	string modelPath = "../yolos/yolov3.weights";
-	string configPath = "../yolos/yolov3.cfg";
-	string inputImg = "../yolos/dog.jpg";
-	Net net = readNet(modelPath, configPath);
-	net.setPreferableBackend(DNN_BACKEND_CUDA);
-	net.setPreferableTarget(DNN_TARGET_CUDA);
+	std::string modelPath = "../yolos/yolov3.weights";
+	std::string configPath = "../yolos/yolov3.cfg";
+	std::string inputImg = "../yolos/dog.jpg";
+	auto net = cv::dnn::readNet(modelPath, configPath);
+	net.setPreferableBackend(cv::dnn::DNN_BACKEND_CUDA);
+	net.setPreferableTarget(cv::dnn::DNN_TARGET_CUDA);
 
-	Mat image = imread(inputImg);
-	Mat blob = blobFromImage(image, 0.00392, Size(416, 416), Scalar(), true, false, CV_32F);
+	cv::Mat image = cv::imread(inputImg);
+	cv::Mat blob = cv::dnn::blobFromImage(image, 0.00392, cv::Size(416, 416), cv::Scalar(), true, false, CV_32F);
 	auto outputNames = net.getUnconnectedOutLayersNames();
-	vector<Mat> detections;
+	std::vector<cv::Mat> detections;
 
 	// warmup
 	for(int i = 0; i < 3; i++)
@@ -38,17 +39,17 @@ int main()
 	}
 
 	// benckmark
-	start = clock();
+	auto start = std::chrono::steady_clock::now();
 	for(int i = 0; i < 100; i++)
 	{
 		net.setInput(blob);
 		net.forward(detections, outputNames);
 	}
-	end = clock();
+	auto end = std::chrono::steady_clock::now();
 
-	double timeTaken = double(end - start) / double(CLOCKS_PER_SEC);
-	cout << "Time per inference: " << timeTaken << " seconds" << endl
-		<< "FPS: " << 1.0 / timeTaken << endl;
+	std::chrono::milliseconds timeTaken = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	std::cout << "Time per inference: " << timeTaken.count() << " ms" << std::endl
+		<< "FPS: " << 1000.0 / (timeTaken.count()) << std::endl;
 
 	return 0;
 }
